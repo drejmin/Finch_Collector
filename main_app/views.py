6db70c3737803d2aa3f49f70b497dfd544d9867c
drejmin <djjamn@gmail.com>
2023-08-15 08:50:22 -0700

    modified:   main_app/admin.py 	new file:   main_app/forms.py 	new file:   main_app/migrations/0002_feeding.py 	modified:   main_app/models.py 	modified:   main_app/templates/base.html 	modified:   main_app/templates/finches/detail.html 	new file:   main_app/templates/main_app/finch_confirm_delete.html 	new file:   main_app/templates/main_app/finch_form.html 	modified:   main_app/urls.py 	modified:   main_app/views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+# from django.views.generic import ListView, DetailView
+from .forms import FeedingForm
 
 from.models import Finch
 
@@ -10,16 +13,41 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
-    return render(request, 'finches/index.html',{
-        'finches': finches
-    })
-
-def finch_index(request):
     finches = Finch.objects.all()
     return render(request,'finches/index.html',{
         'finches': finches
     })
 
-def cats_detail(request, finch_id):
+def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+    feeding_form = FeedingForm()
+    return render( request, 'finches/detail.html',{
+                   'finch': finch,'feeding_form': feeding_form,})
+    
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = ['name', 'breed', 'description', 'age']
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  # create a ModelForm instance using 
+  # the data that was submitted in the form
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # We want a model instance, but
+    # we can't save to the db yet
+    # because we have not assigned the
+    # finch_id FK.
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
